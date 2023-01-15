e8577f0804abc0f933dcfd7b6af693afcacd90fe
Paulo <phm.paulomorais@gmail.com>
2023-01-15 20:35:26 -0300

    Serviço de autorização da API Algafood

---
+++ src/main/java/com/algaworks/algafood/auth/core/AuthorizationServerConfig.java
@@ -0,0 +1,142 @@
+package com.algaworks.algafood.auth.core;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtKeyStoreProperties jwtKeyStoreProperties;
+	
+	// @Autowired
+	// private RedisConnectionFactory redisConnectionFactory;
+	
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients
+			.inMemory()
+				.withClient("algafood-web")
+				.secret(passwordEncoder.encode("web123"))
+				.authorizedGrantTypes("password", "refresh_token")
+				.scopes("WRITE", "READ")
+				.accessTokenValiditySeconds(0 * 60 * 6) // 6 horas (padrão é 12 horas) 
+				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
+				.and()
+					.withClient("foodanalytics")
+					.secret(passwordEncoder.encode("food123"))
+					.authorizedGrantTypes("authorization_code")
+					.scopes("WRITE", "READ")
+					.redirectUris("http://localhost:8082")
+				.and()
+					.withClient("webadmin")
+					.authorizedGrantTypes("implicit")
+					.scopes("WRITE", "READ")
+					.redirectUris("http://aplicacao-cliente")					
+				.and()
+					.withClient("faturamento")
+					.secret(passwordEncoder.encode("faturamento123"))
+					.authorizedGrantTypes("client_credentials")
+					.scopes("WRITE", "READ")
+				.and()
+					.withClient("checktoken")
+					.secret(passwordEncoder.encode("check123"));
+	}
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//		security.checkTokenAccess("isAuthenticated()");
+		security.checkTokenAccess("permitAll()");
+	}
+	
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		var enhancerChain = new TokenEnhancerChain();
+		enhancerChain.setTokenEnhancers(
+				Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
+		
+		endpoints.authenticationManager(authenticationManager)
+					.userDetailsService(userDetailsService)
+					.reuseRefreshTokens(false)
+					.accessTokenConverter(jwtAccessTokenConverter())
+					.tokenEnhancer(enhancerChain)
+					.approvalStore(approvalStore(endpoints.getTokenStore()))
+					//.tokenStore(redisTokenStore())
+					.tokenGranter(tokenGranter(endpoints));
+		
+	}
+	
+	// Ver aula 23.13-revisando-o-fluxo-de-aprovacao-do-authorization-code-com-jwt
+	private ApprovalStore approvalStore(TokenStore tokenStore) {
+		var approvalStore = new TokenApprovalStore();
+		approvalStore.setTokenStore(tokenStore);
+		
+		return approvalStore;
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		// jwtAccessTokenConverter.setSigningKey("89a7sd89f7as98f7dsa98fds7fd89sasd9898asdf98s");
+		
+		 var jksResource = new ClassPathResource(jwtKeyStoreProperties.getPath());
+		 var keyStorePass = jwtKeyStoreProperties.getPassword();
+		 var keyPairAlias = jwtKeyStoreProperties.getKeypairAlias();
+		    
+		 var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+		 var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+		    
+		 jwtAccessTokenConverter.setKeyPair(keyPair);
+		
+		return jwtAccessTokenConverter;
+	}
+	
+	// private TokenStore redisTokenStore() {
+	//	return new RedisTokenStore(redisConnectionFactory);
+	// }
+	
+	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+				endpoints.getOAuth2RequestFactory());
+		
+		var granters = Arrays.asList(
+				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+		
+		return new CompositeTokenGranter(granters);
+	}
+	
+	
+} 
